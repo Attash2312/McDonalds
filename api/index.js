fa420cadf0de00f867fc37ae96066089092e0aad
attash2312 <attash2312@gmail.com>
2025-07-14 05:20:37 +0500

    Final fix: Use Vercel auto-detection

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function entry point
-const app = require('../server');
-
-// Export as a Vercel function handler
-module.exports = (req, res) => {
-  return app(req, res);
-}; 
--- api/test.js
+++
@@ -1,9 +0,0 @@
-// Simple test function
-module.exports = (req, res) => {
-  res.json({
-    message: 'API test function working',
-    timestamp: new Date().toISOString(),
-    method: req.method,
-    url: req.url
-  });
-}; 
